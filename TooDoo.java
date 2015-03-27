deb4d40f7f9ddfddea4cc7831ab4320132ed32cd
Nicholas Schumacher <NTS.716@gmail.com>
2015-03-26 21:25:48 -0700

    switched from using name inputs to numbers, added some error checking

--- TooDoo.java
+++ TooDoo.java
@@ -12,14 +12,29 @@ public class TooDoo {
 	String inp_name = scanner.nextLine();
 	//Get the description of the new task
 	tasks.add(new Task(inp_name));
-	//Confirm the creation of the task
-	System.out.println("A new task has been created");
+	//Confirmation message
+	System.out.println("Done.");
+	}
+
+	public static void help()
+	{
+		System.out.println("\nnew - creates a new task");
+		System.out.println("list - views the created tasks");
+		System.out.println("check - checks a selected task");
+		System.out.println("clean - removes all checked tasks");
+		System.out.println("remove - removes a selected task");
+		System.out.println("help - show this dialouge");
+		System.out.println("quit - exits TooDoo\n");
 	}
 
 	public static void list()
 	{
 		//loops through all of the tasks
 		for (int i = 0; i < tasks.size(); i++) {
+			//Prints the number of the task
+			System.out.print("(");
+			System.out.print(i + 1);
+			System.out.print(") ");
 			//Print out the task name
 			System.out.print(tasks.get(i).name);
 			//Prints the completed check box
@@ -34,16 +49,22 @@ public class TooDoo {
 	public static void check()
 	{
 		//Prompt for the name of the task
-		System.out.print("Enter the name of the task: ");
+		System.out.print("Enter the number of the task: ");
 		String taskToCheck = scanner.nextLine();
-		//Loop through the creates tasks
-		for (int i = 0; i < tasks.size(); i++) {
-			if (taskToCheck.equals(tasks.get(i).name)) {
-				tasks.get(i).completed = true;
-				System.out.println("The task has been checked");
-				break;
-			}
+		//Convert the user input to an integer
+		int taskInt = Integer.parseInt(taskToCheck);
+		//Check the task
+		try {
+
+		tasks.get(taskInt - 1).completed = true;
+
+		} catch (IndexOutOfBoundsException e) {
+
+			System.out.println("\nERROR: task does not exist\n");
+
 		}
+		//Confirmation message
+		System.out.println("Done.");
 	}
 
 	public static void clean()
@@ -58,13 +79,20 @@ public class TooDoo {
 
 	public static void remove()
 	{
-		System.out.print("Enter the name of the task: ");
+		System.out.print("Enter the number of the task: ");
 		String taskToRemove = scanner.nextLine();
-		for (int i = 0; i < tasks.size(); i++) {
-			if (taskToRemove.equals(tasks.get(i).name)) {
-				tasks.remove(i);
-			}
+		int taskInt = Integer.parseInt(taskToRemove);
+		try {
+
+		tasks.remove(taskInt - 1);
+		
+		} catch (IndexOutOfBoundsException e) {
+	
+			System.out.println("\nERROR: task does not exist\n");
+
 		}
+		//Confirmation Message
+		System.out.println("Done.");
 	}
 
 	public static void getInp()
@@ -91,6 +119,8 @@ public class TooDoo {
 
 			} else if (inp.equals("remove")) {
 				remove();
+			} else {
+				help();
 			}
 		}
 	}
@@ -99,6 +129,7 @@ public class TooDoo {
 	{
 		//Welcome Message
 		System.out.println("TooDoo 1.0.0");
+		help();
 		getInp();
 	}
 }
